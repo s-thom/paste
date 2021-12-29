45c940604b5b8d21b996d1b4d8f47b911faae7b7
Stuart Thomson <me@sthom.kiwi>
2021-12-29 19:23:58 +1300

    Copy code from examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+extern crate dotenv;
+extern crate env_logger;
+extern crate log;
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+    env_logger::init();
+
+    log::info!("Hello, world!");
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
