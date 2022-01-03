83ee852d5bde628b4925545688968a0815bf5cff
Stuart Thomson <me@sthom.kiwi>
2022-01-03 19:22:28 +1300

    Add strict content security policy
    Nothing is allowed.

--- src/routes.rs
+++ src/routes.rs
@@ -1,17 +1,20 @@
 use mime::Mime;
-use warp::http::header::{HeaderMap, HeaderValue, AUTHORIZATION};
+use warp::http::header::{
+    HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_SECURITY_POLICY, CONTENT_TYPE,
+    X_CONTENT_TYPE_OPTIONS,
+};
 use warp::{Filter, Rejection, Reply};
 
 use crate::handlers;
 
 pub fn headers_wrapper() -> warp::filters::reply::WithHeaders {
     let mut pastes_headers = HeaderMap::new();
-    pastes_headers.insert("content-type", HeaderValue::from_static("text/plain"));
+    pastes_headers.insert(CONTENT_TYPE, HeaderValue::from_static("text/plain"));
+    pastes_headers.insert(X_CONTENT_TYPE_OPTIONS, HeaderValue::from_static("nosniff"));
     pastes_headers.insert(
-        "x-content-type-options",
-        HeaderValue::from_static("nosniff"),
+        CONTENT_SECURITY_POLICY,
+        HeaderValue::from_static("default-src 'none'"),
     );
-    // TODO: Add strict CSP headers
 
     warp::reply::with::headers(pastes_headers)
 }
@@ -33,7 +36,7 @@ pub fn create_route() -> impl Filter<Extract = impl Reply, Error = Rejection> + 
         .and(warp::post())
         .and(warp::body::content_length_limit(1024 * 1024 * 64))
         .and(warp::header::<String>(AUTHORIZATION.as_str()))
-        .and(warp::header::<Mime>("content-type"))
+        .and(warp::header::<Mime>(CONTENT_TYPE.as_str()))
         .and(warp::body::stream())
         .and_then(handlers::create_handler)
 }
